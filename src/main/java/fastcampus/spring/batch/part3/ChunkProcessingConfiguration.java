0152103f5fe44e4330339891d99d3b73a95f98ff
test <test@test.com>
2023-06-29 22:33:50 +0900

    chunk / tasklet 예제

---
+++ src/main/java/fastcampus/spring/batch/part3/ChunkProcessingConfiguration.java
@@ -0,0 +1,114 @@
+package fastcampus.spring.batch.part3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Configuration
+public class ChunkProcessingConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public ChunkProcessingConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job ChunkProcessingJob() {
+        return jobBuilderFactory.get("ChunkProcessingJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.taskBaseStep())
+                .next(chunkBaseStep(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step chunkBaseStep(@Value("#{jobParameters[chunkSize]}") String chunkSize) {
+        return stepBuilderFactory.get("chunkBaseStep")
+                .<String, String>chunk(Objects.nonNull(chunkSize) ? Integer.parseInt(chunkSize) : 10)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemWriter<String> itemWriter() {
+        return item -> log.info("chunk item size : {}", item.size());
+//        return items -> items.forEach(log::info);
+    }
+
+    private ItemProcessor<String, String> itemProcessor() {
+        return item -> item + ", spring Batch";
+    }
+
+    private ItemReader<? extends String> itemReader() {
+        return new ListItemReader<>(getItems());
+    }
+
+    @Bean
+    public Step taskBaseStep() {
+        return stepBuilderFactory.get("taskBaseStep")
+                .tasklet(this.tasklet())
+                .build();
+    }
+
+    public Tasklet tasklet() {
+        List<String> items = getItems();
+        return (contribution, chunkContext) -> {
+            StepExecution stepExecution = contribution.getStepExecution();
+            JobParameters jobParameters = stepExecution.getJobParameters();
+
+            String value = jobParameters.getString("chunkSize", "10");
+            int chunkSize = Objects.nonNull(value) ? Integer.parseInt(value) : 10;
+            int fromIndex = stepExecution.getReadCount();
+            int toIndex = fromIndex + chunkSize;
+
+            if(fromIndex >= items.size()) {
+                return RepeatStatus.FINISHED;
+            }
+            List<String> subList = items.subList(fromIndex, toIndex);
+
+            log.info("task item size : {}", subList.size());
+
+            stepExecution.setReadCount(toIndex);
+
+            return RepeatStatus.CONTINUABLE;
+        };
+    }
+
+    private List<String> getItems() {
+        List<String> items = new ArrayList<>();
+
+        for (int i = 0; i< 100; i++) {
+            items.add(i + " Hello");
+        }
+        return items;
+    }
+
+
+}
